cb6d6609018d07b8d032ccc888a57770c1937de3
Petr <petr@flaks.dev>
2022-03-10 22:28:51 +0300

    fix: fix ObjectField class

--- src/ObjectField.php
+++ src/ObjectField.php
@@ -8,19 +8,15 @@ use Closure;
 
 /**
  * This class represents a field in an object (associative array).
- *
- * @template TReturnType of mixed
- *
- * @deprecated Use {@see ObjectPropertyDenormalizer} instead. This class will be removed in 1.0.0 release.
  */
 class ObjectField
 {
     /**
-     * @param Closure(mixed, Pointer): TReturnType $denormalizer Denormalizer function that handles denormalization of the field.
-     *                                                           First parameter of the function will contain value of the field.
-     *                                                           The second one will contain {@see Pointer} pointing to the field.
-     * @param bool                                 $isMandatory  Should the field be presented in payload or not
-     * @param bool                                 $isNullable   May the field be `null` or not
+     * @param Closure(mixed, Pointer): mixed $denormalizer Denormalizer function that handles denormalization of the field.
+     *                                                     First parameter of the function will contain value of the field.
+     *                                                     The second one will contain {@see Pointer} pointing to the field.
+     * @param bool                           $isMandatory  Should the field be presented in payload or not
+     * @param bool                           $isNullable   May the field be `null` or not
      */
     public function __construct(
         private Closure $denormalizer,
@@ -34,7 +30,7 @@ class ObjectField
      * First parameter of the function will contain value of the field.
      * The second one will contain {@see Pointer} pointing to the field.
      *
-     * @return Closure(mixed, Pointer): TReturnType
+     * @return Closure(mixed, Pointer): mixed
      */
     public function getDenormalizer(): Closure
     {
